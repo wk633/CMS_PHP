57c0185b3505282479f1bb1213f2d76753cc4544
wk633 <wk633@outlook.com>
2017-06-06 14:23:24 -0700

    动态生成navbar上的categories

--- includes/db.php
+++ includes/db.php
@@ -11,8 +11,8 @@ foreach($db as $key => $value) {
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if ($connection) {
-    echo "connect success";
-}
+//if ($connection) {
+//    echo "connect success";
+//}
 
 ?>
