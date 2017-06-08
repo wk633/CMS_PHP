06bc151a11f968f5a3f129b6f55f1279303be418
wk633 <wk633@outlook.com>
2017-06-08 15:24:06 -0700

    finish categoroy page of admin, display part

--- admin/index.php
+++ admin/index.php
@@ -16,14 +16,35 @@
                         Welcome to admin page
                         <small>Subheading</small>
                     </h1>
-                    <ol class="breadcrumb">
-                        <li>
-                            <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                        </li>
-                        <li class="active">
-                            <i class="fa fa-file"></i> Blank Page
-                        </li>
-                    </ol>
+                    
+                    <div class="col-xs-6">
+                        <form action="">
+                            <div class="form-group">
+                               <label for="">Add categories</label>
+                                <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" name="submit">
+                            </div>
+                        </form>
+                    </div> <!-- add categories form -->
+                    
+                    <div class="col-xs-6">
+                        <table class="table table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Category Title</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>1</th>
+                                    <th>test</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div> <!-- categories table -->
                 </div>
             </div>
             <!-- /.row -->
