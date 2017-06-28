5509024032f6ec8db5b20d1705088c39cd2cf9c0
wk633 <wk633@outlook.com>
2017-06-28 11:28:51 -0700

    create post page

---
+++ admin/includes/add_post.php
@@ -0,0 +1,58 @@
+<?php
+if(isset($_POST['create_post'])){
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_author = $_POST['post_author'];
+    $post_status = $_POST['post_status'];
+    
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    
+    
+    $post_image = $_FILES['post_image']['name'];
+    $post_image_temp = $_FILES['post_image']['tmp_name'];
+    
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+    
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+}
+?>
+
+
+
+<form action="" method='post' enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="post_title">Post Title</label>
+        <input type="text" class='form-control' name='post_title'>
+    </div>
+    <div class="form-group">
+        <label for="post_category_id">Post Category Id</label>
+        <input type="text" class='form-control' name='post_category_id'>
+    </div>
+    <div class="form-group">
+        <label for="post_author">Post Author</label>
+        <input type="text" class='form-control' name='post_author'>
+    </div>
+    <div class="form-group">
+        <label for="post_status">Post Status</label>
+        <input type="text" class='form-control' name='post_status'>
+    </div>
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="file" name='post_image'>
+    </div>
+    <div class="form-group">
+        <label for="post_tags">Post Tags</label>
+        <input type="text" class='form-control' name='post_tags'>
+    </div>
+    <div class="form-group">
+        <label for="post_content">Post Content</label>
+        <textarea class='form-control' name="post_content" id="" cols="30" rows="10"></textarea>
+    </div>
+    
+    <div class="form-group">
+        <input type="submit" value='publish' name='create_post' class='btn btn-primary'>
+    </div>
+</form>
