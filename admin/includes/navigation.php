72c1bee8572d682ff7dfaabeb5ebf7b61ae3ebef
wk633 <wk633@outlook.com>
2017-06-28 10:31:41 -0700

    add posts page and modify navigation link

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -39,7 +39,7 @@
                 <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                 <ul id="posts_dropdown" class="collapse">
                     <li>
-                        <a href="#">View all posts</a>
+                        <a href="posts.php">View all posts</a>
                     </li>
                     <li>
                         <a href="#">Add posts</a>
@@ -48,7 +48,7 @@
             </li>
             
             <li>
-                <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                <a href="categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
             </li>
             
             <li class="active">
---
+++ admin/posts.php
@@ -0,0 +1,64 @@
+<?php include "includes/header.php"; ?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/navigation.php"; ?>
+    
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    <h1 class="page-header">
+                        Welcome to admin page
+                        <small>Subheading</small>
+                    </h1>
+                    
+                    <table class='table table-hover table-bordered'>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Author</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Status</th>
+                                <th>Image</th>
+                                <th>Tags</th>
+                                <th>Comments</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>12</td>
+                                <td>21</td>
+                                <td>32</td>
+                                <td>321</td>
+                                <td>43</td>
+                                <td>432</td>
+                                <td>432</td>
+                                <td>432</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    
+                    
+                    
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include "includes/footer.php"; ?>
