76b999f6515c753360380887dffd2a9efab5b3da
wk633 <wk633@outlook.com>
2017-06-29 10:19:10 -0700

    add links to post

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
                 $query = "SELECT * FROM posts";
                 $select_all_posts = mysqli_query($connection, $query);
                 while($row = mysqli_fetch_assoc($select_all_posts)) {
+                    $post_id = $row['post_id'];
                     $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
@@ -33,7 +34,7 @@
                 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?post_id=<?php echo $post_id;?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
@@ -43,7 +44,7 @@
                 <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
                 <hr>
                 <p><?php echo $post_content; ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <a class="btn btn-primary" href="post.php?post_id=<?php echo $post_id;?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
                 
